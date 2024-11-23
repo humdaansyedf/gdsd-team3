fe4595c30f5949f274651e742b4d6130b8091a25
Zaid Akhter <zaidakhter1202@gmail.com>
2024-11-23 01:15:37 +0100

    search by title

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -1,11 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
+  const [title, setTitle] = useState("");
   const { data, isLoading, error } = useQuery({
-    queryKey: ["property"],
+    queryKey: ["property", { title }],
     queryFn: async () => {
-      const response = await fetch("/api/property");
+      const response = await fetch(`/api/property?title=${title}`);
       const data = await response.json();
       return data;
     },
@@ -14,6 +16,16 @@ export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <form
+        className="search-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setTitle(e.target.title.value);
+        }}
+      >
+        <input name="title" />
+        <button type="submit">Search</button>
+      </form>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {data &&
