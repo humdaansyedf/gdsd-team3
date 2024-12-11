4f8989f4cb90f2b783632a65493f455fed088349
Zaid Akhter <zaidakhter1202@gmail.com>
2024-12-11 10:56:29 +0100

    please

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,6 +1,6 @@
 import { Carousel } from "@mantine/carousel";
 import { Card, Image } from "@mantine/core";
-import classes from "./carousel.module.css";
+import classes from "./Carousel.module.css";
 
 const images = [
   "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
