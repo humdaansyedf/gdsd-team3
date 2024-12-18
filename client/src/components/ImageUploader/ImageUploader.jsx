f2a0cf215951bf591a83cbbbb8d915d7f8a459a6
Humdaan Syed <humdaan.syed@informatik.hs-fulda.de>
2024-12-18 18:17:08 +0100

    Added S3 rollback

--- client/src/components/ImageUploader/ImageUploader.jsx
+++ client/src/components/ImageUploader/ImageUploader.jsx
@@ -1,14 +1,52 @@
 import { useState } from "react";
-import { Text, Image, SimpleGrid } from "@mantine/core";
+import { Text, Image, SimpleGrid, ActionIcon } from "@mantine/core";
 import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { IconTrash } from "@tabler/icons-react";
 import { usePublicFileUpload } from "./fileUpload";
+import axios from "axios";
 
 export function ImageUploader() {
   const [images, setImages] = useState([]);
   const fileUploadMutation = usePublicFileUpload();
-  const previews = images.map((file, index) => {
-    return <Image key={index} src={file.url} />;
-  });
+
+  // Function to delete the image
+  const deleteImage = async (index, url) => {
+    try {
+      const key = url.split("amazonaws.com/")[1]; // Extract S3 key from URL
+      // Call backend endpoint to delete the image from S3
+      await axios.delete("/api/file", {
+        data: { key }, // Send the file URL to identify and delete the S3 object
+      });
+
+      // Remove the image from the frontend state
+      setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+    } catch (error) {
+      console.error("Failed to delete image:", error);
+    }
+  };
+
+// Previews with delete button
+  const previews = images.map((file, index) => (
+    <div key={index} style={{ position: "relative" }}>
+      <Image src={file.url} />
+      <ActionIcon
+        style={{
+          position: "absolute",
+          top: "5px",
+          right: "5px",
+          backgroundColor: "white",
+          borderRadius: "50%",
+        }}
+        onClick={() => deleteImage(index, file.url)} // Trigger delete
+        title="Delete image"
+        size="sm"
+      >
+        <IconTrash 
+          size={16}
+          color = "red" />
+      </ActionIcon>
+    </div>
+  ));
 
   return (
     <div>
