b93d53514a93328e3ef7481b253bf0a254f3773c
Zaid Akhter <zaidakhter1202@gmail.com>
2025-01-20 12:59:18 +0100

    new layout

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -4,58 +4,45 @@ import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 import { useAuth } from "../lib/auth-context";
-import {
-  AuthProvider,
-  PrivateRoute,
-  PublicOnlyRoute,
-} from "../lib/auth-provider";
+import { AuthProvider, PrivateRoute, PublicOnlyRoute } from "../lib/auth-provider";
 import { AdminAuthProvider } from "../lib/admin-auth-provider";
+import { AppLayout } from "../components/AppLayout/AppLayout.jsx";
 
-const Login = React.lazy(() =>
-  import("./login/login-page").then((mod) => ({ default: mod.Login })),
-);
-const Register = React.lazy(() =>
-  import("./register/register-page").then((mod) => ({ default: mod.Register })),
-);
-const Home = React.lazy(() =>
-  import("./home/home-page").then((mod) => ({ default: mod.Home })),
-);
+const Login = React.lazy(() => import("./login/login-page").then((mod) => ({ default: mod.Login })));
+const Register = React.lazy(() => import("./register/register-page").then((mod) => ({ default: mod.Register })));
+const Home = React.lazy(() => import("./home/home-page").then((mod) => ({ default: mod.Home })));
 const LandlordDashboardPage = React.lazy(() =>
   import("./landlord-dashboard/landlord-dashboard-page").then((mod) => ({
     default: mod.LandlordDashboardPage,
-  })),
+  }))
 );
 const CreateAdPage = React.lazy(() =>
   import("./create-ad/create-ad-page.jsx").then((mod) => ({
     default: mod.CreateAdPage,
-  })),
+  }))
 );
 const AdConfirmation = React.lazy(() =>
   import("./ad-confirmation/ad-confirmation-page.jsx").then((mod) => ({
     default: mod.AdConfirmation,
-  })),
+  }))
 );
 const PropertyDetail = React.lazy(() =>
   import("./property-detail/property-detail-page").then((mod) => ({
     default: mod.PropertyDetail,
-  })),
+  }))
 );
-const Profile = React.lazy(() =>
-  import("./profile/profile-page").then((mod) => ({ default: mod.Profile })),
-);
-const Mymessages = React.lazy(() =>
-  import("./messaging/mymessages").then((mod) => ({ default: mod.Mymessages })),
-);
+const Profile = React.lazy(() => import("./profile/profile-page").then((mod) => ({ default: mod.Profile })));
+const Mymessages = React.lazy(() => import("./messaging/mymessages").then((mod) => ({ default: mod.Mymessages })));
 
 const AdminDashboard = React.lazy(() =>
   import("./admin/admin-dashboard-page").then((mod) => ({
     default: mod.AdminDashboard,
-  })),
+  }))
 );
 const AdminProperty = React.lazy(() =>
   import("./admin/admin-property-page").then((mod) => ({
     default: mod.AdminProperty,
-  })),
+  }))
 );
 
 const AppLoader = () => {
@@ -66,29 +53,29 @@ const AppLoader = () => {
   );
 };
 
-const AppLayout = () => {
-  const { isLoading } = useAuth();
+// const AppLayout = () => {
+//   const { isLoading } = useAuth();
 
-  return (
-    <>
-      <div className="disclaimer">
-        Fulda University of Applied Sciences Software Engineering Project, Fall
-        2024. FOR DEMONSTRATION ONLY.
-      </div>
-      {isLoading ? (
-        <AppLoader />
-      ) : (
-        <>
-          <Header />
-          <Container fluid>
-            <Outlet />
-          </Container>
-          <Footer />
-        </>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className="disclaimer">
+//         Fulda University of Applied Sciences Software Engineering Project, Fall
+//         2024. FOR DEMONSTRATION ONLY.
+//       </div>
+//       {isLoading ? (
+//         <AppLoader />
+//       ) : (
+//         <>
+//           <Header />
+//           <Container fluid>
+//             <Outlet />
+//           </Container>
+//           <Footer />
+//         </>
+//       )}
+//     </>
+//   );
+// };
 
 const NotFound = () => {
   return (
@@ -103,8 +90,7 @@ const AdminLayout = () => {
   return (
     <>
       <div className="disclaimer">
-        Fulda University of Applied Sciences Software Engineering Project, Fall
-        2024. FOR DEMONSTRATION ONLY.
+        Fulda University of Applied Sciences Software Engineering Project, Fall 2024. FOR DEMONSTRATION ONLY.
       </div>
       <Container fluid>
         <Outlet />
