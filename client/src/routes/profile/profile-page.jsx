559401996431baf49a73e484e9eab3608361fb24
Zaid Akhter <zaidakhter1202@gmail.com>
2025-01-22 10:53:26 +0100

    profile done

--- client/src/routes/profile/profile-page.jsx
+++ client/src/routes/profile/profile-page.jsx
@@ -1,5 +1,4 @@
-import { Avatar, Box, Button, Card, Group, Text, Title } from "@mantine/core";
-import React from "react";
+import { Avatar, Badge, Button, Container, Flex, Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../lib/auth-context";
 
@@ -11,58 +10,32 @@ export function Profile() {
   }
 
   return (
-    <Card
-      shadow="md"
-      padding="lg"
-      radius="md"
-      style={{
-        maxWidth: 400,
-        margin: "auto",
-        marginTop: "40px",
-        backgroundColor: "#ECF6E9",
-        color: "#000",
-        textAlign: "center",
-      }}
-    >
-      {/* Profile Picture */}
-      <Box
-        style={{ display: "flex", justifyContent: "center", marginBottom: 20 }}
-      >
-        <Avatar size="xl" radius="xl" />
-      </Box>
+    <Container size="xs" mt="xl">
+      <Paper withBorder shadow="md" p="lg">
+        <Stack align="center" Stack spacing="xs">
+          <Avatar size="xl" />
 
-      {/* Type Tag */}
-      <Box style={{ textAlign: "center", marginBottom: 10 }}>
-        <Text
-          size="sm"
-          style={{
-            display: "inline-block",
-            padding: "4px 10px",
-            border: "1px solid #000",
-            borderRadius: 8,
-          }}
-        >
-          {user.type}
-        </Text>
-      </Box>
+          <Badge size="lg" radius="sm" variant="light">
+            {user.type}
+          </Badge>
 
-      {/* Profile Info */}
-      <Title order={3} style={{ fontFamily: "sans-serif", marginBottom: 10 }}>
-        {user.name}
-      </Title>
-      <Text>Email: {user.email || "N/A"}</Text>
-      <Text>Phone: {user.phone || "N/A"}</Text>
-      <Text>Address: {user.address || "N/A"}</Text>
+          <Flex direction="column" align="center">
+            <Title order={3}>{user.name}</Title>
+            <Text>Email: {user.email || "N/A"}</Text>
+            <Text>Phone: {user.phone || "N/A"}</Text>
+            <Text>Address: {user.address || "N/A"}</Text>
+          </Flex>
 
-      {/* Buttons */}
-      <Group position="center" mt="md">
-        {/**<Button variant="outline" size="sm" onClick={() => console.log("Edit clicked!")}>
+          <Group position="center">
+            {/**<Button variant="outline" size="sm" onClick={() => console.log("Edit clicked!")}>
           Edit
         </Button>**/}
-        <Button variant="filled" size="sm" color="red" onClick={logout}>
-          Logout
-        </Button>
-      </Group>
-    </Card>
+            <Button variant="filled" size="md" color="red" onClick={logout}>
+              Logout
+            </Button>
+          </Group>
+        </Stack>
+      </Paper>
+    </Container>
   );
 }
